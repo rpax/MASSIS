c16e841e75e3bc7710c24b06b1ca44b752915175
rpax <pax.rafael@gmail.com>
2015-10-28 20:08:25 +0100

    Created additionalData project
    Helps for storing custom stuff in the home archive

---
+++ sh3d-additionaldata-app/src/main/java/rpax/massis/sweethome3d/additionaldata/AdditionalDataWriterAdapter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpax.massis.sweethome3d.additionaldata;
+
+import com.eteks.sweethome3d.model.Home;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author Rafael Pax
+ */
+public class AdditionalDataWriterAdapter implements AdditionalDataWriter{
+
+    @Override
+    public void writeAdditionalData(Home home, File homeFile) throws IOException
+    {
+    }
+    
+}
