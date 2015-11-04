332c8837d657504ef5d0b36c2ca64dacfb8dde6d
rpax <pax.rafael@gmail.com>
2015-11-04 11:10:07 +0100

    Removed deploy script

--- massis-framework-all/deploy.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-SET DEPLOY_PATH=C:\Users\Rafael Pax\Downloads\temp\maven-repo
-RMDIR /S /Q "%DEPLOY_PATH%"
-mvn clean install deploy -DaltDeploymentRepository=massis.github.repo::default::file://"%DEPLOY_PATH%"
