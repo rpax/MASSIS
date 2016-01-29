1aef443cb3d0865c1faf5708e5c19782ff7f9a07
rpax <pax.rafael@gmail.com>
2016-01-29 14:09:24 +0100

    subfolder to root

--- massis-sh3d-plugins/src/main/java/rpax/massis/sweethome3d/plugins/design/DesignerLog.java
+++
@@ -1,8 +0,0 @@
-package rpax.massis.sweethome3d.plugins.design;
-
-public interface DesignerLog {
-
-	public DesignerLog append(String str);
-	public DesignerLog appendLine(String str);
-	public DesignerLog clear();
-}
